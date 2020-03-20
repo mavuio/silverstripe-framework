89e0504b4a3ac6172e5c67425f00103aa3b7dd62
Manfred Wuits <manfred@monochrom.at>
2020-03-20 23:17:27 +0100

    IntNull fixed

--- src/ORM/Connect/MySQLSchemaManager.php
+++ src/ORM/Connect/MySQLSchemaManager.php
@@ -528,7 +528,18 @@ class MySQLSchemaManager extends DBSchemaManager
         //For reference, this is what typically gets passed to this function:
         //$parts=Array('datatype'=>'int', 'precision'=>11, 'null'=>'not null', 'default'=>(int)$this->default);
         //DB::requireField($this->tableName, $this->name, "int(11) not null default '{$this->defaultVal}'");
-        return "int(11) not null" . $this->defaultClause($values);
+
+
+        // mwuits ---------- BEGIN
+        if ($values['null']=='null') {
+            $notnullclause='null';
+            $defval='NULL';
+        } else {
+            $notnullclause='not null';
+            $defval=(int)$values['default'];
+        }
+        return "int(11) $notnullclause" . $this->defaultClause($values);
+        // mwuits ---------- END
     }
 
     /**
@@ -614,11 +625,11 @@ class MySQLSchemaManager extends DBSchemaManager
     }
 
     /*
-	 * Return the MySQL-proprietary 'Year' datatype
-	 *
-	 * @param array $values Contains a tokenised list of info about this data type
-	 * @return string
-	 */
+     * Return the MySQL-proprietary 'Year' datatype
+     *
+     * @param array $values Contains a tokenised list of info about this data type
+     * @return string
+     */
     public function year($values)
     {
         return 'year(4)';
